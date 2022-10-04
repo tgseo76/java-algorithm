ee7adf5fcd0a3f29eaf47724b6b7babd28ff5d2e
tjxor <seotaegeon@gmail.com>
2022-10-04 13:40:34 +0900

    화폐계산

--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -5,13 +5,21 @@ public class ArrayTest {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
 
-        int[] arr=new int[10];
+        System.out.print("금액을 넣으세요:[숫자입력] ");
+        int money=input.nextInt();
+        int[] don = {50000,10000,5000,1000,500,100,50,10};
 
-        for (int i=0;i<arr.length;i++){
-            arr[i]= input.nextInt();
-            }
+        for (int i=0;i<don.length;i++){
+            int cnt=0;
 
-        System.out.println(Arrays.toString(arr));
+            cnt=money/don[i];
+            money=money-(don[i]*cnt);
+
+
+            System.out.println(don[i]+"원"+cnt+"개");
+
+        }
+
     }
 
 }
