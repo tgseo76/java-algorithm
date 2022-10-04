7104d9fde88ebc59c05f049da63eb6ce97132d6c
tjxor <seotaegeon@gmail.com>
2022-10-04 13:21:37 +0900

    배열 10개만들기

---
+++ src/ArrayTest.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+        int[] arr=new int[10];
+
+        for (int i=0;i<arr.length;i++){
+            arr[i]= input.nextInt();
+            }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
