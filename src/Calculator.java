f19b76896c6885b21339de93fd69ba842373c81d
tjxor <seotaegeon@gmail.com>
2022-10-05 10:49:12 +0900

    Calculator divide 생성자 수정

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,17 +1,25 @@
 public class Calculator {
-    int plus(int a, int b){
+    private int a;
+    private int b;
+
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    int plus(){
         return a+b;
     }
 
-    int minus(int a, int b){
+    int minus(){
         return a-b;
     }
 
-    int multiple(int a, int b){
+    int multiple(){
         return a*b;
     }
 
-    float divide(int a, int b){
+    float divide(){
         return (float)a/b;
     }
 }
--- src/CalculatorMain.java
+++ src/CalculatorMain.java
@@ -1,12 +1,11 @@
 public class CalculatorMain {
     public static void main(String[] args) {
-        Calculator cal = new Calculator();
+        Calculator cal = new Calculator(5,2);
 
-        int a = 5;
-        int b = 2;
-        System.out.println(cal.plus(a,b));
-        System.out.println(cal.minus(a,b));
-        System.out.println(cal.multiple(a,b));
-        System.out.println(cal.divide(a,b));
+
+        System.out.println(cal.plus());
+        System.out.println(cal.minus());
+        System.out.println(cal.multiple());
+        System.out.println(cal.divide());
     }
 }
