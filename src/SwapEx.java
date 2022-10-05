e0816466702038641df412951bd4aee72858e3c2
tjxor <seotaegeon@gmail.com>
2022-10-05 09:21:00 +0900

    swap ex

---
+++ src/SwapEx.java
@@ -0,0 +1,16 @@
+public class SwapEx {
+    public static void main(String[] args) {
+
+        int tem,a,b;
+
+        a=5;
+        b=10;
+
+        tem=a;
+        a=b;
+        b=tem;
+
+        System.out.printf("%d, %d", a, b);
+
+    }
+}
