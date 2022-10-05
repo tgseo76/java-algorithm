427ff4c59beb4afbec31d617806373293af4ddcf
tjxor <seotaegeon@gmail.com>
2022-10-05 10:19:59 +0900

    Calculator 실습

---
+++ src/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    int plus(int a, int b){
+        return a+b;
+    }
+
+    int minus(int a, int b){
+        return a-b;
+    }
+
+    int multiple(int a, int b){
+        return a*b;
+    }
+
+    int divide(int a, int b){
+        return a/b;
+    }
+}
