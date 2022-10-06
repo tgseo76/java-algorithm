64fd5fe9df69f46b3a8f435be8b4fbe1eede2d99
tjxor <seotaegeon@gmail.com>
2022-10-06 13:21:30 +0900

    알파벳인지 확인

--- src/Ex1006/RanNumExMain.java
+++ src/Ex1006/RanNumExMain.java
@@ -4,19 +4,19 @@ import java.util.HashSet;
 
 public class RanNumExMain {
     public static void main(String[] args) {
-        RanNumEx ranNumEx=new RanNumEx();
+//        RanNumEx ranNumEx=new RanNumEx();
+        char r;
 
         HashSet<Character> numbers = new HashSet<>();
         for(int i=0;i<30;i++){
-            char r;
-            int num=(int)(Math.random()*(26)+65);
+
+            int num=(int)(Math.random()*(90-65+1)+65);
             r = (char)num;
             numbers.add(r);
         }
 
         System.out.println(numbers);
         System.out.println(numbers.size());
-
 
     }
 }
