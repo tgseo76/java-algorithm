8daf41bd23dff312b2ac8fff3898fd4bc23df7f4
tjxor <seotaegeon@gmail.com>
2022-10-17 09:08:13 +0900

    별찍기4단

---
+++ src/Ex1017/RightTriangle.java
@@ -0,0 +1,13 @@
+package Ex1017;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+
+        for (int i=0;i<4;i++){
+            for (int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
