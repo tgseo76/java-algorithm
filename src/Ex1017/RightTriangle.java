482b04a6669e8652a05821a97e4240862fc84bd9
tjxor <seotaegeon@gmail.com>
2022-10-17 09:32:10 +0900

    별찍기 도전과제

--- src/Ex1017/RightTriangle.java
+++ src/Ex1017/RightTriangle.java
@@ -3,21 +3,35 @@ package Ex1017;
 import java.util.Scanner;
 
 public class RightTriangle {
-    void print(int n){
-        for (int i=0;i<n;i++){
-            for (int j=0;j<=i;j++){
+    void print(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.print("\n");
         }
     }
 
+    void printNew(int n,char c){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.printf("%c",c);
+            }
+            System.out.print("\n");
+        }
+    }
+
+
     public static void main(String[] args) {
-        RightTriangle rightTriangle=new RightTriangle();
-        Scanner input=new Scanner(System.in);
-        int n= input.nextInt();
-        rightTriangle.print(n);
+        RightTriangle rightTriangle = new RightTriangle();
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        char c = input.next().charAt(0);
+//        rightTriangle.print(n);
+        rightTriangle.printNew(n,c);
 
 
     }
 }
+
+
