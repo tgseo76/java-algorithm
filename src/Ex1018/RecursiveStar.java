35b659543a3a71c4dc3a92e474967d9045bf62bf
tjxor <seotaegeon@gmail.com>
2022-10-18 09:23:24 +0900

    재귀함수 별찍기

---
+++ src/Ex1018/RecursiveStar.java
@@ -0,0 +1,25 @@
+package Ex1018;
+
+import java.util.Scanner;
+
+public class RecursiveStar {
+    public void rs(int a,int b){
+        if (a==b+1){
+            return;
+        }
+        else {
+            System.out.println("*".repeat(a));
+            rs(a+1,b);
+        }
+    }
+    public static void main(String[] args) {
+        RecursiveStar rs=new RecursiveStar();
+        Scanner input=new Scanner(System.in);
+        int n= input.nextInt();
+        rs.rs(1,n);
+
+
+
+
+    }
+}
