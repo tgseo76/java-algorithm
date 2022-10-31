88d3116647fb2084e28ba83042e3cddebaab6536
tjxor <seotaegeon@gmail.com>
2022-10-31 09:32:19 +0900

    모의고사 fc

---
+++ src/ex1031/모의고사.java
@@ -0,0 +1,26 @@
+package ex1031;
+
+public class 모의고사 {
+//    https://school.programmers.co.kr/learn/courses/30/lessons/42840
+public int[] solution(int[] answers) {
+
+//    1번 : 1, 2, 3, 4, 5, 1, 2, 3, 4, 5,
+//    2번 : 2, 1, 2, 3, 2, 4, 2, 5, 2, 1,
+//    3번 : 3, 3, 1, 1, 2, 2, 4, 4, 5, 5,
+
+//    패턴
+
+    // 1번 ((i%5)+1)
+    // 2번
+//     3번
+    int[] answer = {};
+
+    return new int[0];
+}
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
