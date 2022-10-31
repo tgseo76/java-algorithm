9640b345ac614bb957b9cf20b8227b8ca90afacf
tjxor <seotaegeon@gmail.com>
2022-10-31 09:44:06 +0900

    모의고사 패턴찾기

--- src/ex1031/모의고사.java
+++ src/ex1031/모의고사.java
@@ -2,17 +2,22 @@ package ex1031;
 
 public class 모의고사 {
 //    https://school.programmers.co.kr/learn/courses/30/lessons/42840
-public int[] solution(int[] answers) {
-
-//    1번 : 1, 2, 3, 4, 5, 1, 2, 3, 4, 5,
+    //    1번 : 1, 2, 3, 4, 5, 1, 2, 3, 4, 5,
 //    2번 : 2, 1, 2, 3, 2, 4, 2, 5, 2, 1,
 //    3번 : 3, 3, 1, 1, 2, 2, 4, 4, 5, 5,
 
-//    패턴
+    /*    패턴
 
-    // 1번 ((i%5)+1)
-    // 2번
-//     3번
+         1번 ((i%5)+1)
+         2번 i%2==0 i=2
+            i%2!=0 i=
+
+       3번
+
+     */
+public int[] solution(int[] answers) {
+
+
     int[] answer = {};
 
     return new int[0];
@@ -20,6 +25,11 @@ public int[] solution(int[] answers) {
 
 
     public static void main(String[] args) {
+    int[] s1={1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+        int[] s2={2, 1, 2, 3, 2, 4, 2, 5, 2, 1};
+        int[] s3={3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+
 
     }
 
