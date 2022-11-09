2e4f396be06dc1c37792768b9866aa961c2a9639
tjxor <seotaegeon@gmail.com>
2022-11-09 09:38:06 +0900

    선택정렬 완성

--- src/ex1109/SelectionSort.java
+++ src/ex1109/SelectionSort.java
@@ -7,16 +7,18 @@ public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {3,7,4,9,10,223,111,23,2,39};
 
-        int i=0;
-        int minIdx=i;
-        for (int j = i+1; j < arr.length; j++) {
-            if (arr[i]>arr[j]){
-                minIdx=j;
+        for (int i = 0; i < arr.length-1; i++) {
+            int minIdx=i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i]>arr[j]){
+                    minIdx=j;
+                }
             }
+            int tem=arr[i];
+            arr[i]=arr[minIdx];
+            arr[minIdx]=tem;
         }
-        int tem=arr[i];
-        arr[i]=arr[minIdx];
-        arr[minIdx]=tem;
+
 
         System.out.println(Arrays.toString(arr));
 
