d1b451557b5663517f3f5599c779d81de30a3a61
tjxor <seotaegeon@gmail.com>
2022-11-09 09:43:43 +0900

    선택정렬 내림차순

--- src/ex1109/SelectionSort.java
+++ src/ex1109/SelectionSort.java
@@ -6,17 +6,33 @@ public class SelectionSort {
 
     public static void main(String[] args) {
         int[] arr = {3,7,4,9,10,223,111,23,2,39};
+//내림차순
+//        for (int i = 0; i < arr.length-1; i++) {
+//            int minIdx=i;
+//            for (int j = i+1; j < arr.length; j++) {
+//                if (arr[i]>arr[j]){
+//                    minIdx=j;
+//                }
+//            }
+//            int tem=arr[i];
+//            arr[i]=arr[minIdx];
+//            arr[minIdx]=tem;
+//        }
+//
+//
+//        System.out.println(Arrays.toString(arr));
 
+        //오름차순
         for (int i = 0; i < arr.length-1; i++) {
-            int minIdx=i;
+            int maxIdx=i;
             for (int j = i+1; j < arr.length; j++) {
-                if (arr[i]>arr[j]){
-                    minIdx=j;
+                if (arr[maxIdx]<arr[j]){
+                    maxIdx=j;
                 }
             }
             int tem=arr[i];
-            arr[i]=arr[minIdx];
-            arr[minIdx]=tem;
+            arr[i]=arr[maxIdx];
+            arr[maxIdx]=tem;
         }
 
 
