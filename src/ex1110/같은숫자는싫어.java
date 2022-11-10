7dd5ea054185d7e0d02b68f6c5f6ec76ce5c36a8
tjxor <seotaegeon@gmail.com>
2022-11-10 09:28:57 +0900

    같은 숫자는 싫어 useStack 완료

--- src/ex1110/같은숫자는싫어.java
+++ src/ex1110/같은숫자는싫어.java
@@ -4,6 +4,7 @@ package ex1110;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 /*
 https://school.programmers.co.kr/learn/courses/30/lessons/12906
@@ -26,6 +27,24 @@ public class 같은숫자는싫어 {
         }
         return answer;
     }
+
+    public int[] useStack(int []arr) {
+Stack<Integer> arr2=new Stack<>();
+        arr2.push(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+        arr2.push(arr[i]);
+        if (arr[i-1]==arr[i]){
+            arr2.pop();
+        }
+    }
+    int[] answer = new int[arr2.size()];
+        for (int i = 0; i < arr2.size(); i++) {
+        answer[i]=arr2.get(i);
+    }
+        return answer;
+    }
+
+
     public static void main(String[] args) {
         int[] arr = {4,4,4,3,3};
 
