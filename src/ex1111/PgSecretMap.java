7b593de2e822176bbdfe67cc4771517453d0664e
tjxor <seotaegeon@gmail.com>
2022-11-11 09:11:49 +0900

    비밀지도 어떻게 풀어야 할지 주석 추가

---
+++ src/ex1111/PgSecretMap.java
@@ -0,0 +1,13 @@
+package ex1111;
+
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/17681
+구현방법
+1. 입력받은 숫자를 2진법으로 변환
+2. 같은 인덱스 별로 ||연산
+3. replace로 1==# 0==' '로 바꾸기
+ */
+
+
+public class PgSecretMap {
+}
