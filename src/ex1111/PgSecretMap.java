67f26e647261b916b9c6147e25c79fefb742c54c
tjxor <seotaegeon@gmail.com>
2022-11-11 10:36:38 +0900

    비밀지도 성공

--- src/ex1111/PgSecretMap.java
+++ src/ex1111/PgSecretMap.java
@@ -9,5 +9,48 @@ https://school.programmers.co.kr/learn/courses/30/lessons/17681
  */
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PgSecretMap {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        List<Integer> newArr1=new ArrayList<>();
+        String[] answer=new String[n];
+
+        for (int i = 0; i < n; i++) {
+            newArr1.add(arr1[i]|arr2[i]); //[31, 21, 29, 19, 31]
+            String a =Integer.toBinaryString(newArr1.get(i));
+            String b = "0".repeat(n-a.length())+a;
+            b=b.replace('1','#');
+            b=b.replace('0',' ');
+            answer[i]=b;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n=5;
+        int[] arr1 = {9,20,28,18,11};
+        int[] arr2 = {30,1,21,17,28};
+        List<Integer> newArr1=new ArrayList<>();
+        String[] answer=new String[n];
+
+        for (int i = 0; i < n; i++) {
+            newArr1.add(arr1[i]|arr2[i]); //[31, 21, 29, 19, 31]
+            String a =Integer.toBinaryString(newArr1.get(i));
+            String b = "0".repeat(n-a.length())+a;
+            b=b.replace('1','#');
+            b=b.replace('0',' ');
+            answer[i]=b;
+        }
+
+        System.out.println(Arrays.toString(answer));
+
+
+
+
+
+
+    }
 }
