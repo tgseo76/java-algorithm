01e296a8c0e3b38db3a504be9a69254b3f9d378a
tjxor <seotaegeon@gmail.com>
2022-11-17 01:32:53 +0900

    퀵정렬

---
+++ src/ex1116/QuickSort.java
@@ -0,0 +1,55 @@
+package ex1116;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public void sort(int[] arr,int st,int en){ //st=0 //en=arr.length-1
+        if (st>=en){ // st와en이 같다면 배열안에 1개의 값만 들어있다 ==> 탈출조건
+            return;
+        }
+        int pivot = st; //기준값을 각배열의 인덱스0으로 설정
+        int l = st+1; //st는 기준값이므로 다음인덱스부터 정렬
+        int r = en;
+
+
+
+//엇갈리면 while문 종료
+        while (l<=r){
+            while(l<=en && arr[l]<arr[pivot]){
+// 배열의 왼쪽에서부터 pivot보다 큰값을 찾을때까지 l++;
+                l+=1;
+            }
+            while(r>st && arr[pivot]<=arr[r]){
+// 배열의 오른쪽에서부터 pivot보다 작은값을 찾을때까지 r--;
+                r-=1;
+            }
+
+            if(l>r){  // 엇갈렸으면 r과 pivot의 위치를 바꾼다
+// [5,4,2,0,3,1,6,9,7,8] ==> [1,4,2,0,3,5,6,9,7,8]
+                int tem= arr[pivot];
+                arr[pivot]=arr[r];
+                arr[r]=tem;
+            }else { // 엇갈리지 않았으면 l과 r의 위치를 바꾼다
+//[5,7,9,0,3,1,6,2,4,8] ==> [5,4,9,0,3,1,6,2,7,8]
+                int tem=arr[l];
+                arr[l]=arr[r];
+                arr[r]=tem;
+            }
+
+//							[2,1,3,4,5,9,7,6,8]
+            sort(arr,st,r-1);  //분할 후 pivot의 왼쪽 부분
+            sort(arr,r+1,en); //분할 후 pivot의 오른쪽 부분
+//							[2,1,3,4,5,9,7,6,8]
+        }
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,3,2,8,6,2,4};
+
+        QuickSort q = new QuickSort();
+        q.sort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+//           출력값==> [1, 2, 2, 3, 4, 6, 8]
+
+    }
+}
